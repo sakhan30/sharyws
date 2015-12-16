27f16706dac54cbe95c11395b3396991f8dc05a0
shary <sakhan30@yahoo.com>
2015-12-16 17:17:53 -0600

    removed two files

--- src/test/com/tgt/src/TestWeatherService.java
+++ src/test/com/tgt/src/TestWeatherService.java
@@ -1,17 +1,8 @@
 package com.tgt.src;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.*;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import org.junit.Test;
 
 public class WeatherService 
 {
@@ -22,12 +13,10 @@ public class WeatherService
 		{
 			System.out.println("Reached Get Weather");
 
-			String url = "http://www.geoplugin.net/json.gp";
+			String url = "http://localhost:8080/sharyws/weather/1";
 		    HttpClient client = new DefaultHttpClient();
 		    HttpPost post = new HttpPost(url);
-			post.addHeader("ip", "192.172.1.0");
 			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-			urlParameters.add(new BasicNameValuePair("ip", "192.100.0.4"));
 			post.setEntity(new UrlEncodedFormEntity(urlParameters));
          
 			HttpResponse response = client.execute(post);

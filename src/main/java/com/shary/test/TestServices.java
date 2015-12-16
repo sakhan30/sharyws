5cddbd61142f7ce8c14e7dd49461c2f11eef7142
shary <sakhan30@yahoo.com>
2015-12-16 17:45:52 -0600

    Updated test scripts

--- src/main/java/com/shary/test/TestServices.java
+++ src/main/java/com/shary/test/TestServices.java
@@ -1,9 +1,12 @@
 package com.shary.test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
@@ -15,28 +18,32 @@ public class TestServices
 		    DefaultHttpClient httpclient = new DefaultHttpClient();
 		    try 
 		    {
-		      HttpHost target = new HttpHost("localhost", 8080, "http");
-		      HttpGet getRequest = new HttpGet("/sharyws/weather/80020");
-		      System.out.println("executing request to " + target);
-		      HttpResponse httpResponse = httpclient.execute(target, getRequest);
-		      System.out.println("----------------------------------------");
-		      System.out.println(httpResponse.getStatusLine());
-		      Header[] headers = httpResponse.getAllHeaders();
-		      for (int i = 0; i < headers.length; i++) {
-		        System.out.println(headers[i]);
-		      }
-		      System.out.println("----------------------------------------");
+//		      HttpHost target = new HttpHost("localhost", 8080, "http");
+//		      HttpGet getRequest = new HttpGet("/sharyws/weather/80020");
+//		      System.out.println("executing request to " + target);
+//		      HttpResponse httpResponse = httpclient.execute(target, getRequest);
+//		      System.out.println("---1------------------------------------");
+//		      System.out.println(httpResponse.getStatusLine());
+//		      Header[] headers = httpResponse.getAllHeaders();
+//		      for (int i = 0; i < headers.length; i++) {
+//		        System.out.println(headers[i]);
+//		      }
+//		      System.out.println("---2-------------------------------------");
+
+		      HttpClient client = new DefaultHttpClient();
+		      HttpGet request = new HttpGet("http://localhost:8080/sharyws/weather/55445");
+		      HttpResponse response = client.execute(request);
+		      BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+		      String line = "";
+		      while ((line = rd.readLine()) != null)
+              System.out.println("XXX" + line);
+
+		      System.out.println("---3-------------------------------------");
+
 		    } 
 		    catch (Exception e) 
 		    {
 		      e.printStackTrace();
 		    } 
-		    finally 
-		    {
-		      // When HttpClient instance is no longer needed,
-		      // shut down the connection manager to ensure
-		      // immediate deallocation of all system resources
-		      httpclient.getConnectionManager().shutdown();
-		    }
-		  }
+		}
 }
